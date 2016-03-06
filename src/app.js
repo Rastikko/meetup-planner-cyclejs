ac9888483c1b0c958b99c08ed4853be2092365c3
Rastikko <rastikko@gmail.com>
2016-03-05 16:38:28 -0800

    Adding firebase and updating package.json

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import Cycle from '@cycle/core';
 import {makeDOMDriver} from '@cycle/dom';
 import Rx from 'rx';
 import Main from './main'
+import {makeFirebaseDriver} from 'cycle-firebase'
+import Firebase from 'firebase'
 
 // we are pulling in our css files here for webpack to compile
 require("!style!css!styles/layout.css");
@@ -17,7 +19,8 @@ function mainApp(sources) {
 // DOM is the ID or class we want the cycle to render onto our page
 // History is using our makeHistoryDriver to deal with routing
 const sources = {
-  DOM: makeDOMDriver('#application')
+  DOM: makeDOMDriver('#application'),
+  firebase: makeFirebaseDriver(new Firebase('https://meetup-planner.firebaseio.com')),
 };
 
 Cycle.run(mainApp,sources);
